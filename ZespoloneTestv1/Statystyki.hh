7e9694b7c55a02d4265fa64b3a61cfb1fb7680de
PPotoczek <252946@student.pwr.edu.pl>
2020-03-22 20:43:39 +0100

    Strumienie, napisane Wyrazenie Zespolone i poprawiona statystyka

---
+++ ZespoloneTestv1/Statystyki.hh
@@ -0,0 +1,18 @@
+#ifndef STATYSTYKI_HH
+#define STATYSTYKI_HH
+
+struct Statystyki
+{
+    int poprawne;
+    int wszystkie;
+};
+
+int Niepoprawne(Statystyki &Statystyki);
+
+int Procent_popr_odp(Statystyki &Statystyki);
+
+void dodaj_popr(Statystyki &Statystyki);
+
+void dodaj_wszystkie(Statystyki &Statystyki);
+
+#endif //STATYSTYKI_HH
---
+++ ZespoloneTestv1/cmake-build-debug/CMakeFiles/ZespoloneTest.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZespoloneTest.dir/BazaTestu.cpp.o"
+  "CMakeFiles/ZespoloneTest.dir/LZespolona.cpp.o"
+  "CMakeFiles/ZespoloneTest.dir/Statystyki.cpp.o"
+  "CMakeFiles/ZespoloneTest.dir/WyrazenieZesp.cpp.o"
+  "CMakeFiles/ZespoloneTest.dir/main.cpp.o"
+  "ZespoloneTest"
+  "ZespoloneTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZespoloneTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
